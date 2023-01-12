3d226fb2482d7b81b12c207d3d16ee6e8d52de59
fn <1490253514@qq.com>
2023-01-12 21:29:50 +0800

    项目初始å提交

---
+++ gulimall-coupn/src/main/java/com/atguigu/gulimall/coupn/GulimallCoupnApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.coupn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCoupnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallCoupnApplication.class, args);
+	}
+
+}
---
+++ gulimall-coupn/src/test/java/com/atguigu/gulimall/coupn/GulimallCoupnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.coupn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCoupnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
